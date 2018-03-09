3e6b7bc59a6cd7c0c38bc1f4cc0c1c8ed104950c
丁偉哲 <d.weizhe@gmail.com>
2018-03-09 20:16:49 +0800

    最終版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs/Subject";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/mergeMap";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  public sessions: any;
+  public movies: any;
+  public selected = new Subject<any>();
+
+  constructor(public http: HttpClient) {
+    this.getMovieList();
+  }
+
+  async getMovieList() {
+    this.movies = this.http.get("http://movie-api.gosu.bar/api/movie/findAll");
+
+    this.sessions = this.selected.flatMap(movie =>
+      this.http.get("http://movie-api.gosu.bar/api/session/" + movie.id)
+    );
+  }
 }
